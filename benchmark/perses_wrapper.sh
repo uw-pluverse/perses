eb69175cc714fc0ce6ed3b14729437cb15f5d652
Chengnian Sun <chengniansun@gmail.com>
2020-02-06 23:37:15 -0500

    add a new script to help benchmarking

--- benchmark/perses_wrapper.sh
+++ benchmark/perses_wrapper.sh
@@ -3,6 +3,7 @@
 set -o pipefail
 set -o errexit
 set -o nounset
+set -o xtrace
 
 readonly DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 bazel build //src/org/perses:perses_deploy.jar
