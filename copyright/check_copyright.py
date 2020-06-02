76c9f3feddabdc6d33350f25b0e971b0a1e3ae48
XUEYANZ <zxy11256@gmail.com>
2020-06-01 22:40:25 -0400

    Update check_copyright.py
    Updates:
    - imported unittest and os
    - added unit test (java and kt files)
    - added function 'getFiles', which returns a list of files with specified extension
    Comments:
    Hi Dr. Sun, the example was so helpful. After changes above, I was able to run it locally on my PC. 
    It was capable of searching through all 81 java files and 32 kotlin files under the folder 'src' (I downloaded the repo on my PC to run this test). Test result shows 'FAILED' as expected. To confirm its accuracy, I printed out the name of files that lead to test failure, and checked those files manually . It turns out that those files indeed show no copyright info on the top. (e.g. "...\src\org\perses\antlr\AbstractAntlrGrammar.kt")
    
    Function 'getFiles' could be more compact and blended with test cases. (e.g. return a big list of all java and kt files). However, I assume the generality (the ability to take a specified extension) could be potentially useful in the future. (Don't think my program will be actually used in the future, but just trying to advertising my coding mindset.)
    
    Since I imported 'unittest' and 'os', I believe I need to reflect these changes under 'deps' in BUILD file accordingly, right?
    Also, I am not familiar with running 'bazel test //copyrght/...' on github. I am going to learn about this after this file is approved. And will fork repo for Step 4, so I can run test on github whenever.
    
    Appreciate your help. Thanks!

--- copyright/check_copyright.py
+++ copyright/check_copyright.py
@@ -1,8 +1,39 @@
-def check_copyright (sourcefile):
-    """
-    :type sourcefile: String
-    :rtype: Boolean
+import unittest
+import os
 
+src_folder = "//src"
+
+class test_copyright (unittest.TestCase):
+
+    def testcopyright_java(self):
+        javafiles = getFiles(src_folder, "java")
+        for javaf in javafiles:
+            self.assertTrue(check_copyright(javaf))
+
+    def testcopyright_kt(self):
+        ktfiles = getFiles(src_folder, "kt")
+
+        for ktf in ktfiles:
+            if check_copyright(ktf) != True:
+                print(ktf)
+            self.assertTrue(check_copyright(ktf))
+
+
+def getFiles (directory: str, extension: str)->list:
+    """
+    This function takes a directory path and desired extension
+    then returns a list of path+files with a specified extension 
+    """
+    Filelist = list()
+    for root, dirs, files in os.walk(directory):
+        for f in files:
+            if f.endswith('.'+extension):
+                Filelist.append(os.path.join(root,f))
+    return Filelist
+
+
+def check_copyright (sourcefile: str) -> bool:
+    """
     This function takes a source file name and
     outputs true if the file has copyright infomation
     """
@@ -12,10 +43,9 @@ def check_copyright (sourcefile):
     with open("copyright.txt") as file:
         buf_cprt = file.read()
         
-
     """Extract comment block contents"""
     with open(sourcefile) as file:
-        buf_srcfile = file.readline()
+        buf_srcfile = file.readline().rstrip('\n')
         if buf_srcfile.find("/*") == -1:
             return False
         cmtblk=[]
