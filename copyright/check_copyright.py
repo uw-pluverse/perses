6af4b97610cb3c6855f9e54d7f778214dbc4f860
Chengnian Sun <chengniansun@gmail.com>
2020-06-03 11:41:23 -0400

    remove check_copyright.py from build system

--- copyright/check_copyright.py
+++ copyright/check_copyright.py
@@ -1,13 +1,14 @@
+#!/usr/bin/env python3
+
 import unittest
 import os
 
-SRC_FOLDER = 'src'
+SRC_FOLDER = '../src'
 
 """Get copyright info"""
-with open("copyright/copyright.txt") as file:
+with open("copyright.txt") as file:
     BUF_CPRT = file.read()
 BUF_CPRT = ''.join(BUF_CPRT.split())
-
 
 
 class TestCopyright(unittest.TestCase):
@@ -15,28 +16,30 @@ class TestCopyright(unittest.TestCase):
     Test if copyright info exist in files with specified extensions
     """
 
-    def testcopyright_java(self):
+    def test_copyright_java(self):
         """ Test Java files """
         javafiles = get_files(SRC_FOLDER, "java")
+        self.assertTrue(len(javafiles) > 0)
         for javaf in javafiles:
             self.assertTrue(check_copyright(javaf), msg=javaf)
 
-    def testcopyright_kt(self):
+    def test_copyright_kt(self):
         """ Test Kotlin files """
         ktfiles = get_files(SRC_FOLDER, "kt")
         for ktf in ktfiles:
             self.assertTrue(check_copyright(ktf), msg=ktf)
 
 
-def get_files(directory: str, extension: str)->list:
+def get_files(directory: str, extension: str) -> list:
     """
     This function takes a directory path and desired extension
     then returns a list of path+files with specified extension
     """
     file_list = list()
+
     for root, dirs, files in os.walk(directory):
         for file in files:
-            if file.endswith('.'+extension):
+            if file.endswith('.' + extension):
                 file_list.append(os.path.join(root, file))
     return file_list
 
@@ -57,9 +60,11 @@ def check_copyright(sourcefile: str) -> bool:
 
             buf_srcfile = file.readline()
 
-
     buf_srcfile = ''.join(cmtblk)
     buf_srcfile = ''.join(buf_srcfile.split())
     if buf_srcfile.find(BUF_CPRT) == -1:
         return False
     return True
+
+if __name__ == '__main__':
+    unittest.main()
