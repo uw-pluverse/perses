d9e8792edef3fd944d4cc855a21e98eb9e67de64
XUEYANZ <zxy11256@gmail.com>
2020-06-07 13:42:52 -0400

    Update check_copyright_test.py

--- copyright/check_copyright_test.py
+++ copyright/check_copyright_test.py
@@ -2,52 +2,81 @@
 
 import os
 import unittest
+import tempfile
 import check_copyright as c
 
 
+EXT = ['java', 'kt']
 SAMPLE_MSG = ["This is a sample message.\n"] *3
 SAMPLE_MSG_CMTBLK = "/*\n" + " * This is a sample message.\n" *3 + ' */\n'
 
-FOLDERS = ["dum_dir1", "dum_dir2"]
-FILES = ['dum_dir1\dummy1.java', 'dum_dir2\dummy2.java']
 
+class TestCheckCopyright(unittest.TestCase):
 
-class test_check_copyright(unittest.TestCase):
+    ## Temp java, kt files
+    j1 = None
+    k1 = None
+    j1_base = None
+    k1_base = None
+    folder = None
+    
+    def setUp(self):
+        with tempfile.NamedTemporaryFile(suffix='.java', delete=False) as self.j1:
+            print(self.j1.name)
+            self.j1.write(b"import java.util.*;")
+        with tempfile.NamedTemporaryFile(suffix='.kt', delete=False) as self.k1:
+            print(self.k1.name)
+            self.k1.write(b"import platform.posix.*;")
 
+        self.j1_base = os.path.basename(self.j1.name)
+        self.k1_base = os.path.basename(self.k1.name)
+        
+        self.folder = os.path.dirname(self.j1.name)
+        print(self.folder)
+        
+
+    def tearDown(self):
+        os.unlink(self.j1.name)
+        os.unlink(self.k1.name)
+        print(os.path.exists(self.j1.name))
+
+        
     def test_get_files(self):
-        for i, val in enumerate(FOLDERS):
-            l = c.get_files(val, "java")
-            self.assertEqual(l[0], FILES[i])
+        ## get_files(directory: str, extension: str) -> List[str]:
+        l = list()
+        for e in EXT:
+            l += c.get_files(self.folder, e)
+        self.assertTrue(self.j1.name in l)
+        self.assertTrue(self.k1.name in l)
+
 
     def test_check_copyright(self):
-        for file in FILES:
-            self.assertFalse(c.check_copyright(file, SAMPLE_MSG))
+        ## check_copyright(sourcefile: str, rawtext: list) -> bool:
+        self.assertFalse(c.check_copyright(self.j1.name, SAMPLE_MSG))
+        self.assertFalse(c.check_copyright(self.k1.name, SAMPLE_MSG))
+
 
     def test_check_folder(self):
-        l = c.check_folder(os.getcwd(), "java", SAMPLE_MSG)
-        for i, val in enumerate(l):
-            self.assertTrue(FILES[i] in val)
+        ## check_folder(folder: str, extension: str, rawtext: list)->list:
+        l = list()
+        for e in EXT:
+            l += c.check_folder(self.folder, e, SAMPLE_MSG)
+        self.assertTrue(self.j1.name in l)
+        self.assertTrue(self.k1.name in l)
+
 
     def test_comment_block_factory(self):
+        ## comment_block_factory(rawtext: list)->str:
         self.assertEqual(c.comment_block_factory(SAMPLE_MSG), SAMPLE_MSG_CMTBLK)
 
+
     def test_update_files(self):
-        c.update_files(FILES, SAMPLE_MSG)
-        for file in FILES:
-            self.assertTrue(c.check_copyright(file, SAMPLE_MSG))
+        ## update_files(files: list, rawtext: list):
+        c.update_files([self.j1.name, self.k1.name], SAMPLE_MSG)
+        self.assertTrue(c.check_copyright(self.j1.name, SAMPLE_MSG))
+        self.assertTrue(c.check_copyright(self.k1.name, SAMPLE_MSG))
 
 
 if __name__ == '__main__':
 
-    for file in FILES:
-        os.makedirs(os.path.dirname(file), exist_ok = True)
-        with open(file, "w+") as f:
-            f.write("import java.util.*;")
-    with open("dummy3.txt", 'w+') as f:
-        f.write(''.join(SAMPLE_MSG))
-
-    unittest.main(exit=False)
-
-    for file in FILES:
-        os.remove(file)
-    os.remove("dummy3.txt")
+    unittest.main()
