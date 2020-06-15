b0ac13d92578cb18c5b008db9119ccf9c5c79be2
XUEYANZ <zxy11256@gmail.com>
2020-06-14 20:19:56 -0400

    Update check_copyright.py

--- copyright/check_copyright.py
+++ copyright/check_copyright.py
@@ -8,13 +8,20 @@ from typing import List
 SRC_FOLDER = '../src'
 
 
-class Copyright:
-    
+class CopyrightChecker:
+    """CopyrightChecker class offers functions to check all files with
+    specified extension and update corresponding files
+
+    Attributes:
+       copyright_text: A list read from copyright.txt
+    """
+
     def __init__(self, copyright_text):
-        self.buf_cprt = copyright_text
+        """Inits CopyrightChecker with a list version of copyright text"""
+        self.copyright_text = copyright_text
 
 
-    def check_folder(self, folder: str, extension: str)->list:
+    def check_folder(self, folder: str, extension: str) -> List[str]:
         """
         Return a list of files missing copyright
         """
@@ -33,47 +40,45 @@ class Copyright:
 
     def check_copyright(self, sourcefile: str) -> bool:
         """
-        This function takes a source file name and
-        outputs true if the file has copyright infomation
+        Return a boolean indicating if the file has copyright infomation
         """
-        copyright_text = ''.join(self.buf_cprt)
-        copyright_text = ''.join(copyright_text.split())
+        buf_copyright = ''.join(self.copyright_text)
+        buf_copyright = ''.join(buf_copyright.split())
 
         with open(sourcefile) as file:
             buf_srcfile = file.readline().rstrip('\n')
             if buf_srcfile.find("/*") == -1:
                 return False
-            cmtblk = list()
+            comment_block = list()
             buf_srcfile = file.readline()
             while buf_srcfile.find("*/") == -1:
-                cmtblk.append(buf_srcfile.strip(" *"))
+                comment_block.append(buf_srcfile.strip(" *"))
                 buf_srcfile = file.readline()
 
-        buf_srcfile = ''.join(cmtblk)
+        buf_srcfile = ''.join(comment_block)
         buf_srcfile = ''.join(buf_srcfile.split())
 
-        if buf_srcfile.find(copyright_text) == -1:
+        if buf_srcfile.find(buf_copyright) == -1:
             return False
         return True
 
 
     def update_files(self, files: list):
         """ Updates give files with copyright info """
-##        print('Updating files ...')
-        copyright_text = self.buf_cprt[:]
+        buf_copyright = self.copyright_text[:]
 
-        for i, val in enumerate(copyright_text):
-            copyright_text[i] = " * " + val
-        copyright_text.insert(0, "/*\n")
-        copyright_text.append(" */\n")
+        for i, val in enumerate(buf_copyright):
+            buf_copyright[i] = " * " + val
+        buf_copyright.insert(0, "/*\n")
+        buf_copyright.append(" */\n")
 
-        cmtblk = ''.join(copyright_text)
-        
+        comment_block = ''.join(buf_copyright)
+
         for file in files:
-            with open(file, 'r+') as f:
-                content = f.read()
-                f.seek(0, 0)
-                f.write(cmtblk+content)
+            with open(file, 'r+') as target_file:
+                content = target_file.read()
+                target_file.seek(0, 0)
+                target_file.write(comment_block + content)
 
 
 if __name__ == '__main__':
@@ -89,18 +94,18 @@ if __name__ == '__main__':
 
     with open("copyright.txt") as file:
         copyright_text = file.readlines()
-    copyright_checker = Copyright(copyright_text)
+    copyright_checker = CopyrightChecker(copyright_text)
 
     extensions = ['java', 'kt']
     missing_list = list()
-    
+
     for ext in extensions:
         missing_list += copyright_checker.check_folder(SRC_FOLDER, ext)
 
     if not UPDATE_FLAG and missing_list != []:
         raise Exception('\n'.join(missing_list))
 
-    elif UPDATE_FLAG:
+    if UPDATE_FLAG and missing_list != []:
         copyright_checker.update_files(missing_list)
         print('All files now have copyright info')
 
