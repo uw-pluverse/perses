863d72dd9ce3a543bbcc1de3bd6486a64b451d2d
Chengnian Sun <chengniansun@gmail.com>
2020-06-15 20:53:45 -0400

    Convert ParserFacadeTest to kotlin.

--- test/org/perses/grammar/ParserFacadeTest.kt
+++ test/org/perses/grammar/ParserFacadeTest.kt
@@ -1,135 +1,121 @@
-package org.perses.grammar;
+package org.perses.grammar
 
-import com.google.common.collect.ImmutableList;
-import com.google.common.truth.Truth;
-import org.antlr.v4.runtime.Token;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.JUnit4;
-import org.perses.TestUtility;
-import org.perses.antlr.ParseTreeWithParser;
-import org.perses.grammar.c.CParserFacade;
-import org.perses.grammar.c.PnfCParserFacade;
-import org.perses.program.PersesToken;
-import org.perses.program.TokenizedProgram;
-import org.perses.program.TokenizedProgramFactory;
-import org.perses.tree.spar.SparTree;
-import org.perses.tree.spar.SparTreeBuilder;
+import com.google.common.collect.ImmutableList
+import com.google.common.truth.Truth
+import org.antlr.v4.runtime.Token
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+import org.perses.TestUtility
+import org.perses.grammar.c.CParserFacade
+import org.perses.grammar.c.PnfCParserFacade
+import org.perses.program.PersesToken
+import org.perses.program.TokenizedProgram
+import org.perses.tree.spar.SparTreeBuilder
+import java.io.File
+import java.io.IOException
 
-import java.io.File;
-import java.io.IOException;
-
-import static com.google.common.truth.Truth.assertThat;
-
-@RunWith(JUnit4.class)
-public class ParserFacadeTest {
-
-  private CParserFacade facade = new CParserFacade();
-  private PnfCParserFacade pnfcFacade = new PnfCParserFacade();
+@RunWith(JUnit4::class)
+class ParserFacadeTest {
+  private val facade = CParserFacade()
+  private val pnfcFacade = PnfCParserFacade()
 
   @Test
-  public void testTokenize() throws IOException {
-    ImmutableList<Token> tokens = facade.parseIntoTokens(new File("test_data/misc/t1.c"));
-    ImmutableList<String> tokenTexts =
-        tokens.stream().map(Token::getText).collect(ImmutableList.toImmutableList());
-    assertThat(tokenTexts)
-        .containsExactly("int", "a", ";", "int", "b", ";", "int", "a", ",", "b", ";")
-        .inOrder();
+  @Throws(IOException::class)
+  fun testTokenize() {
+    val tokens = facade.parseIntoTokens(File("test_data/misc/t1.c"))
+    val tokenTexts = tokens.stream().map { obj: Token -> obj.text }.collect(ImmutableList.toImmutableList())
+    Truth.assertThat(tokenTexts)
+      .containsExactly("int", "a", ";", "int", "b", ";", "int", "a", ",", "b", ";")
+      .inOrder()
   }
 
   @Test
-  public void testIsParsable_true() throws IOException {
-    final TokenizedProgram program =
-        TestUtility.createSparTreeFromFile("test_data/misc/t1.c").getProgramSnapshot();
-    assertThat(facade.isSourceCodeParsable(program.toSourceCodeInOrigFormatWithBlankLines()))
-        .isTrue();
-    assertThat(facade.isSourceCodeParsable(program)).isTrue();
-
-    assertThat(pnfcFacade.isSourceCodeParsable(program.toSourceCodeInOrigFormatWithBlankLines()))
-        .isTrue();
-    assertThat(pnfcFacade.isSourceCodeParsable(program)).isTrue();
-
-    final TokenizedProgram invalidProgram = deriveInvalidProgram(program);
-    assertThat(facade.isSourceCodeParsable(invalidProgram.toSourceCodeInOrigFormatWithBlankLines()))
-        .isFalse();
-    assertThat(facade.isSourceCodeParsable(invalidProgram)).isFalse();
-
-    assertThat(
-            pnfcFacade.isSourceCodeParsable(
-                invalidProgram.toSourceCodeInOrigFormatWithBlankLines()))
-        .isFalse();
-    assertThat(pnfcFacade.isSourceCodeParsable(invalidProgram)).isFalse();
+  fun testIsParsable_true() {
+    val program = TestUtility.createSparTreeFromFile("test_data/misc/t1.c").programSnapshot
+    Truth.assertThat(facade.isSourceCodeParsable(program.toSourceCodeInOrigFormatWithBlankLines()))
+      .isTrue()
+    Truth.assertThat(facade.isSourceCodeParsable(program)).isTrue()
+    Truth.assertThat(pnfcFacade.isSourceCodeParsable(program.toSourceCodeInOrigFormatWithBlankLines()))
+      .isTrue()
+    Truth.assertThat(pnfcFacade.isSourceCodeParsable(program)).isTrue()
+    val invalidProgram = deriveInvalidProgram(program)
+    Truth.assertThat(facade.isSourceCodeParsable(invalidProgram.toSourceCodeInOrigFormatWithBlankLines()))
+      .isFalse()
+    Truth.assertThat(facade.isSourceCodeParsable(invalidProgram)).isFalse()
+    Truth.assertThat(
+      pnfcFacade.isSourceCodeParsable(
+        invalidProgram.toSourceCodeInOrigFormatWithBlankLines()))
+      .isFalse()
+    Truth.assertThat(pnfcFacade.isSourceCodeParsable(invalidProgram)).isFalse()
   }
 
   @Test
-  public void testParseTokenizedProgram_t1() throws IOException {
-    testParseTokenizedProgram("t1.c");
+  fun testParseTokenizedProgram_t1() {
+    testParseTokenizedProgram("t1.c")
   }
 
   @Test
-  public void testParseTokenizedProgram_t2() throws Exception {
-    testParseTokenizedProgram("t2.c");
+  fun testParseTokenizedProgram_t2() {
+    testParseTokenizedProgram("t2.c")
   }
 
   @Test
-  public void testParseTokenizedProgram_t3() throws Exception {
-    testParseTokenizedProgram("t3.c");
+  fun testParseTokenizedProgram_t3() {
+    testParseTokenizedProgram("t3.c")
   }
 
   @Test
-  public void testParseTokenizedProgram_t4() throws Exception {
-    testParseTokenizedProgram("t4.c");
+  fun testParseTokenizedProgram_t4() {
+    testParseTokenizedProgram("t4.c")
   }
 
   @Test
-  public void testParseTokenizedProgram_t5() throws Exception {
-    testParseTokenizedProgram("t5.c");
+  fun testParseTokenizedProgram_t5() {
+    testParseTokenizedProgram("t5.c")
   }
 
   @Test
-  public void testParseTokenizedProgram_t9() throws Exception {
-    testParseTokenizedProgram("t9.c");
+  fun testParseTokenizedProgram_t9() {
+    testParseTokenizedProgram("t9.c")
   }
 
-  private void testParseTokenizedProgram(String filename) throws IOException {
-    final SparTree sparTreeFromFile =
-        TestUtility.createSparTreeFromFile("test_data/misc/" + filename);
-    final TokenizedProgram originalProgram = sparTreeFromFile.getProgramSnapshot();
-    final ImmutableList<String> originalTokens = toAntlrTokens(originalProgram.getTokens());
-    TokenizedProgramFactory factory = sparTreeFromFile.getTokenizedProgramFactory();
-    {
-      final ParseTreeWithParser parseTreeFromFacade = facade.parseTokenizedProgram(originalProgram);
-      final SparTree tree =
-          new SparTreeBuilder(facade.getRuleHierarchy(), factory)
-              .build(parseTreeFromFacade.getTree());
-      final ImmutableList<String> tokens = toAntlrTokens(tree.getProgramSnapshot().getTokens());
-      Truth.assertThat(tokens).containsExactlyElementsIn(originalTokens).inOrder();
+  private fun testParseTokenizedProgram(filename: String) {
+    val sparTreeFromFile = TestUtility.createSparTreeFromFile("test_data/misc/$filename")
+    val originalProgram = sparTreeFromFile.programSnapshot
+    val originalTokens = toAntlrTokens(originalProgram.tokens)
+    val factory = sparTreeFromFile.tokenizedProgramFactory
+    run {
+      val (tree1) = facade.parseTokenizedProgram(originalProgram)
+      val tree = SparTreeBuilder(facade.ruleHierarchy, factory)
+        .build(tree1)
+      val tokens = toAntlrTokens(tree.programSnapshot.tokens)
+      Truth.assertThat(tokens).containsExactlyElementsIn(originalTokens).inOrder()
     }
-    {
-      final ParseTreeWithParser parseTreeFromPnfc =
-          pnfcFacade.parseTokenizedProgram(originalProgram);
-      final SparTree treeFromPnfc =
-          new SparTreeBuilder(pnfcFacade.getRuleHierarchy(), factory)
-              .build(parseTreeFromPnfc.getTree());
-      final ImmutableList<String> tokens =
-          toAntlrTokens(treeFromPnfc.getProgramSnapshot().getTokens());
-      Truth.assertThat(tokens).containsExactlyElementsIn(originalTokens).inOrder();
+    run {
+      val (tree) = pnfcFacade.parseTokenizedProgram(originalProgram)
+      val treeFromPnfc = SparTreeBuilder(pnfcFacade.ruleHierarchy, factory)
+        .build(tree)
+      val tokens = toAntlrTokens(treeFromPnfc.programSnapshot.tokens)
+      Truth.assertThat(tokens).containsExactlyElementsIn(originalTokens).inOrder()
     }
   }
 
-  private static ImmutableList<String> toAntlrTokens(ImmutableList<PersesToken> tokens) {
-    return tokens.stream().map(Token::getText).collect(ImmutableList.toImmutableList());
-  }
+  companion object {
+    private fun toAntlrTokens(tokens: ImmutableList<PersesToken>): ImmutableList<String?> {
+      return tokens.stream().map { obj: PersesToken -> obj.text }.collect(ImmutableList.toImmutableList())
+    }
 
-  private static TokenizedProgram deriveInvalidProgram(TokenizedProgram program) {
-    ImmutableList.Builder<PersesToken> builder = ImmutableList.builder();
-    for (PersesToken t : program.getTokens()) {
-      final String lexeme = t.getText();
-      if (lexeme.equals(";") || lexeme.equals(",")) {
-        continue;
+    private fun deriveInvalidProgram(program: TokenizedProgram): TokenizedProgram {
+      val builder = ImmutableList.builder<PersesToken>()
+      for (t in program.tokens) {
+        val lexeme = t.text
+        if (lexeme == ";" || lexeme == ",") {
+          continue
+        }
+        builder.add(t)
       }
-      builder.add(t);
+      return TokenizedProgram(builder.build())
     }
-    return new TokenizedProgram(builder.build());
   }
 }
