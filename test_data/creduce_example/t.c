5d7d5eecfef939e557b6f368ed8d987201228594
Chengnian Sun <chengniansun@gmail.com>
2020-08-03 19:17:49 -0400

    Add a new test subject for creduce.

--- test_data/creduce_example/t.c
+++ test_data/creduce_example/t.c
@@ -1,2 +1,14 @@
-int printf(const char *, ...);
-int main() { printf("world\n"); }
+int printf(const char*, ...);
+
+void fun() {
+  int a = 1;
+  int b = a + 1;
+  printf("%d\n", b);
+  printf("hello\n");
+  printf("world\n");
+}
+
+int main (int argc, char *argv[]) {
+  fun();
+  return 0;
+}
