6d22971c307f7197a3ae0f3660ad54c8a3bcea73
Chengnian Sun <chengniansun@gmail.com>
2020-08-03 01:30:17 -0400

    test Shell.sh

--- test/org/perses/util/ShellTest.kt
+++ test/org/perses/util/ShellTest.kt
@@ -1,5 +1,7 @@
 package org.perses.util
 
+import com.google.common.base.Preconditions
+import com.google.common.truth.Truth
 import org.junit.Assert
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -11,13 +13,31 @@ import java.lang.IllegalStateException
 class ShellTest {
   @Test
   fun lsExists() {
-    Shell.ensureCommandExecutable("ls", File("."))
+    val normalized = Shell.normalizeAndCheckExecutability("ls")
+    Truth.assertThat(normalized).isEqualTo("ls")
   }
 
   @Test
   fun doesNotExist() {
     Assert.assertThrows(IllegalStateException::class.java) {
-      Shell.ensureCommandExecutable("this_shell_cmd_does_not_exist", File("."))
+      Shell.normalizeAndCheckExecutability("this_shell_cmd_does_not_exist")
     }
+  }
+
+  @Test
+  fun localScript() {
+    val tempFile = File.createTempFile("test", "run.sh")
+    Preconditions.checkState(tempFile.setExecutable(true))
+    tempFile.deleteOnExit()
+
+    val normalized = Shell.normalizeAndCheckExecutability(tempFile.toString())
+    Truth.assertThat(normalized).isEqualTo(tempFile.toString())
+  }
+
+  @Test
+  fun localRelativeScript() {
+    val cmd = "test/org/perses/util/fake_creduce.sh"
+    val normalized = Shell.normalizeAndCheckExecutability(cmd)
+    Truth.assertThat(normalized).contains(cmd)
   }
 }
