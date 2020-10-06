c8829057546959fbbd3b5dcf7c5afe7094687cfc
Chengnian Sun <chengniansun@gmail.com>
2020-10-06 00:39:56 -0400

    add a script to update the bazel in the docker image

---
+++ benchmark/binaries/update_bazel_in_docker.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -o nounset
+set -o pipefail
+set -o errexit
+
+sudo apt install curl gnupg
+curl -fsSL https://bazel.build/bazel-release.pub.gpg | gpg --dearmor > bazel.gpg
+sudo mv bazel.gpg /etc/apt/trusted.gpg.d/
+echo "deb [arch=amd64] https://storage.googleapis.com/bazel-apt stable jdk1.8" | sudo tee /etc/apt/sources.list.d/bazel.list
+
+sudo apt update && sudo apt install bazel
+
+export PATH="/usr/bin/bazel:${PATH}"
