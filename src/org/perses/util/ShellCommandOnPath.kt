0d4123c729d449f3d1d4c30e7ffd1aaeccdf1d60
Chengnian Sun <chengniansun@gmail.com>
2020-10-09 16:17:21 -0400

    work on ShellCommandOnPath

--- src/org/perses/util/ShellCommandOnPath.kt
+++ src/org/perses/util/ShellCommandOnPath.kt
@@ -1,5 +1,23 @@
+/*
+ * Copyright (C) 2018-2020 University of Waterloo.
+ *
+ * This file is part of Perses.
+ *
+ * Perses is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 3, or (at your option) any later version.
+ *
+ * Perses is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * Perses; see the file LICENSE.  If not see <http://www.gnu.org/licenses/>.
+ */
 package org.perses.util
 
+import com.google.common.collect.ImmutableList
+import com.google.common.collect.ImmutableMap
 import com.google.common.flogger.FluentLogger
 import java.io.File
 import java.nio.file.Files
@@ -9,7 +27,8 @@ import java.nio.file.Paths
  * This class represents a command in the shell.
  */
 data class ShellCommandOnPath(
-  val command: String) {
+  val command: String,
+  val defaultFlags: ImmutableList<String> = ImmutableList.of()) {
 
   private val path = Paths.get(command)
 
@@ -35,6 +54,21 @@ data class ShellCommandOnPath(
       }
       check(result) { "The command $command cannot be found on PATH. $pathEnv" }
     }
+  }
+
+  fun runWith(
+    extraArguments: ImmutableList<String>,
+    workingDirectory: File,
+    captureOutput: Boolean, environment:
+    ImmutableMap<String, String>
+  ): Shell.CmdOutput {
+
+    val cmd = StringBuilder()
+    cmd.append(command).append(' ')
+    defaultFlags.joinTo(cmd, separator = " ")
+    cmd.append(' ')
+    extraArguments.joinTo(cmd, separator = " ")
+    return Shell.run(cmd.toString(), workingDirectory, captureOutput, environment)
   }
 
   companion object {
