9cd2e1226cca36f5cd36b6a3b30e653734ab93d2
XueyanZhang <zxy11256@gmail.com>
2020-10-14 10:58:29 -0400

    Add PERSES_MEMORY_PROFILER to benchmark.py

--- benchmark/binaries/run_perses.sh
+++ benchmark/binaries/run_perses.sh
@@ -19,11 +19,25 @@ readonly REDUCED_TOKEN_FILE="${TMP}/reduced.c"
 
 pushd "${TMP}"
 readonly START_TIME=$(date +%s)
-java -jar "/tmp/binaries/perses_deploy.jar" \
+java -jar "/tmp/binaries/perses_deploy.jar" ${PERSES_EXTRA_FLAGS:=''}\
   --test-script "${TMP_TEST_SCRIPT}" \
   --input-file "${TMP_SOURCE_FILE}" \
   --output-file "${TMP}/reduced.c" \
-  --threads "${THREADS}" ${PERSES_EXTRA_FLAGS:-""}
+  --threads "${THREADS}" &
+readonly PERSES_PID=$!
+
+
+if [ -n ${PERSES_MEMORY_PROFILER:=''}]
+then
+	readonly MEMORY_PROFILER_DIR="$(dirname "$BINARY_DIR")"
+	echo "	*****PERSES_PID=${PERSES_PID}*****"
+	echo "	Memory usage logging into ${MEMORY_PROFILER_DIR}/${PERSES_MEMORY_PROFILER}"
+	#Take samples at 60 seconds interval until process exit
+	jstat -gccapacity -t -h30 ${PERSES_PID} 60000 >> ${MEMORY_PROFILER_DIR}/${PERSES_MEMORY_PROFILER}
+fi
+
+
+wait ${PERSES_PID}
 readonly RET_CODE="$?"
 readonly END_TIME=$(date +%s)
 
