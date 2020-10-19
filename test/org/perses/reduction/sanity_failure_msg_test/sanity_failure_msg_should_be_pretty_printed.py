9c82941c03b97cf03ab7bcd860b4519dc3a820c5
XueyanZhang <zxy11256@gmail.com>
2020-10-19 19:11:09 -0400

    sanity check update #42

--- test/org/perses/reduction/sanity_failure_msg_test/sanity_failure_msg_should_be_pretty_printed.py
+++ test/org/perses/reduction/sanity_failure_msg_test/sanity_failure_msg_should_be_pretty_printed.py
@@ -5,8 +5,12 @@ class TestCheckCopyright(unittest.TestCase):
 
     def test_sanity_failure_msg_should_be_pretty_printed(self):
         with open("test/org/perses/reduction/sanity_failure_msg_test/sanity_failure_log.txt") as f:
-            # TODO: Xueyan: test your code here.
-            pass
+            error_message = ''.join(f.read().split())
+        start = error_message.find("*")
+        end = error_message.rfind("*")
+        result = error_message[start:end].find("Theinitialsanitycheckfailed")
+        flag = False if result == -1 else True
+        self.assertTrue(flag)
 
 
 if __name__ == '__main__':
