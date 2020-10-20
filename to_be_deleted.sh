51aee0ffbe81c88ad60b9e1e0ddc2b4bc524048c
Chengnian Sun <chengniansun@gmail.com>
2020-10-19 23:58:08 -0400

    [rust grammar]: refactor: extract 'bare_function_type'

--- to_be_deleted.sh
+++ to_be_deleted.sh
@@ -7,3 +7,8 @@ set -o errexit
 
 bazel build test/org/perses/grammar/rust:generate_rust_parser_faillist
 cp bazel-bin/test/org/perses/grammar/rust/rust_parser_failed_files.txt test/org/perses/grammar/rust/golden_fail_list.txt
+
+bazel build //test/org/perses/grammar/rust:cp_pnf_rust_grammar
+cp bazel-bin/test/org/perses/grammar/rust/pnf_rust_grammar.g4 test/org/perses/grammar/rust/golden_pnf_rust.g4
+
+bazel test //test/org/perses/grammar/rust/...
