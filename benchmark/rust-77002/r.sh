cc1e46b70ef5e728fd034b9cde9677b91cf3f603
Chengnian Sun <chengniansun@gmail.com>
2020-11-05 03:09:51 +0000

    refine r.sh of rust-77002

--- benchmark/rust-77002/r.sh
+++ benchmark/rust-77002/r.sh
@@ -9,17 +9,17 @@ readonly RUSTC_VERSION="nightly-2020-09-22"
 rustup toolchain install "${RUSTC_VERSION}"
 
 readonly EXE_WRONG="./wrong.out"
-if ! rustup run "${RUSTC_VERSION}" rustc -Z mir-opt-level=2 -o "${EXE_WRONG}" "${FILE}"; then
+if ! timeout -s 9 60 rustup run "${RUSTC_VERSION}" rustc -Z mir-opt-level=2 -o "${EXE_WRONG}" "${FILE}"; then
   exit 1
 fi
 
 readonly EXE_CORRECT="./correct.out"
-if ! rustup run "${RUSTC_VERSION}" rustc -Z mir-opt-level=1 -o "${EXE_CORRECT}" "${FILE}" ; then
+if ! timeout -s 9 60 rustup run "${RUSTC_VERSION}" rustc -Z mir-opt-level=1 -o "${EXE_CORRECT}" "${FILE}" ; then
   exit 1
 fi
 
 readonly EXE_CORRECT_2="./correct_2.out"
-if ! rustup run "${RUSTC_VERSION}" rustc -Z mir-opt-level=0 -o "${EXE_CORRECT_2}" "${FILE}" ; then
+if ! timeout -s 9 60 rustup run "${RUSTC_VERSION}" rustc -Z mir-opt-level=0 -o "${EXE_CORRECT_2}" "${FILE}" ; then
   exit 1
 fi
 
@@ -27,13 +27,13 @@ readonly OUTPUT_WRONG="wrong_output.txt"
 readonly OUTPUT_CORRECT_1="correct_output.txt"
 readonly OUTPUT_CORRECT_2="correct_output_2.txt"
 
-"${EXE_WRONG}" &> "${OUTPUT_WRONG}"
+timeout -s 9 30 "${EXE_WRONG}" &> "${OUTPUT_WRONG}"
 echo "$?" >> "${OUTPUT_WRONG}"
 
-"${EXE_CORRECT}" &> "${OUTPUT_CORRECT_1}"
+timeout -s 9 30 "${EXE_CORRECT}" &> "${OUTPUT_CORRECT_1}"
 echo "$?" >> "${OUTPUT_CORRECT_1}"
 
-"${EXE_CORRECT_2}" &> "${OUTPUT_CORRECT_2}"
+timeout -s 9 30 "${EXE_CORRECT_2}" &> "${OUTPUT_CORRECT_2}"
 echo "$?" >> "${OUTPUT_CORRECT_2}"
 
 if ! diff -q "${OUTPUT_CORRECT_1}" "${OUTPUT_CORRECT_2}" ; then 
