bc44861cb4baf6186d9e719d27b35828627c1f8c
Sun Gyu Park <spark42996@gmail.com>
2023-10-21 12:35:07 -0400

    Probability Adjustment
    Function added for probability adjustment.

--- naive-probDD/probDD_run_and_test.py
+++ naive-probDD/probDD_run_and_test.py
@@ -1,15 +1,34 @@
+# "elements" is the list of elements to be reduced.
+# "targets" is the list of element indices that are excluded subsequence.
+# "result" is the test outcome if it satisfied the test or not with exclusion.
+def AdjustProbs(elements, targets, result):
+
+    if result == 1:
+        for i in targets:
+            elements[i][0] = 0
+            elements[i][1] = 0
+    elif result == 0:
+        product = 1.0
+        for j in targets:
+            product *= ((1.0-elements[j][1]))
+        for i in targets:
+            if elements[i][0] == 0:
+                continue
+            elements[i][1] /= (1 - product)
+
 def probDD(sequence, test_function):
     # probabilities of each element in the optimal solution
     probabilities = sequence.copy()
     # current optimal solution
     next_test_sequence = sequence.copy()
 
-    while ...:
+    while CheckCondition(probabilities) is False:
         # calculate gain and find the next test to run
 
         # run the test
 
         # modify probabilities based on test result
+
     
     # return the elements with probability 1
 
